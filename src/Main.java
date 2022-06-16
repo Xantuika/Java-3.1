5b7544bdcbb4be5218a6710afa2d9a0546c5d1c0
varvara <kva2208@yandex.ru>
2022-06-16 16:59:02 +0300

    Add files

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int amountTicket = a;
+        int costTicket  = 5000;
+        int orderAmount = ( 5000 * a );
+        int bonus = ( orderAmount /20);
+        System.out.println ( "Hello! Thank you for choosing our airline for your flight. Your bonus is: "+ bonus +" miles" );
+
+    }
+}

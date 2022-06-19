0e614c92d92b246f50154712455d9f849cde7163
Xantuika <105634214+Xantuika@users.noreply.github.com>
2022-06-19 17:02:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 5;
-        int amountTicket = a;
-        int costTicket  = 5000;
-        int orderAmount = ( 5000 * a );
-        int bonus = ( orderAmount /20);
-        System.out.println ( "Hello! Thank you for choosing our airline for your flight. Your bonus is: "+ bonus +" miles" );
+        int amountTicket = 5;
+        int costTicket = 5000;
+        int orderAmount = (costTicket * amountTicket);
+        int bonus = (orderAmount / 20);
+        System.out.println("Hello! Thank you for choosing our airline for your flight. Your bonus is: " + bonus + " miles");
 
     }
 }
